1da2300448e07d77ec54358e25c927e88957ea3d
Fabian Zoffel <fabian.zoffel@stepping-stone.ch>
2018-07-03 16:26:43 +0200

    Blame Louis command

---
+++ commands/blamevonux.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+	    message.channel.send(`${message.author.username} Louis was hast du wieder angestellt, immer machst du Unsinn!`).catch(console.error);
+}
