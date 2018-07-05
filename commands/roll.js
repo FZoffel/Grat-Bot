3d5666292aab051aba0dc786aa05dc607527f775
Fabian Zoffel <fabian.zoffel@stepping-stone.ch>
2018-07-05 16:47:23 +0200

    roll command

---
+++ commands/roll.js
@@ -0,0 +1,5 @@
+exports.run = (client, message, args) => {
+	var roll = Math.floor(Math.random() * 100) + 1;
+	    message.reply("Du hast eine " + roll + " gewürfelt!").catch(console.error);
+		    
+}
