cf7074e33e4df6e018028a2d98b5b7a8d0880733
Fabian Zoffel <fabian.zoffel@stepping-stone.ch>
2018-07-23 14:29:39 +0200

    finished blame.js

--- commands/blame.js
+++ commands/blame.js
@@ -1,4 +1,10 @@
 exports.run = (client, message, args) => {
-	    message.channel.send(`(client.users.get(9988).Mention()`).catch(console.error);
-		    
+var allusers = Array.from(message.guild.members);
+var rand = (allusers[Math.floor(Math.random() * allusers.length)]);
+var string = String(rand);
+var user = string.replace(/.*,/g,"");
+const feelsgreat = client.emojis.find("name", "feelsgreat");
+
+message.channel.send(user + " ist meiner Meinung nach Schuld! " + feelsgreat).catch(console.error);
+
 }
