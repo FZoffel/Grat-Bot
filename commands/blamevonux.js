68519e0c1dea70bbaf40e51ada2975d00eec6bb0
Fabian Zoffel <fabian.zoffel@stepping-stone.ch>
2018-07-23 14:30:00 +0200

    Added variation to blamevonux

--- commands/blamevonux.js
+++ commands/blamevonux.js
@@ -1,3 +1,6 @@
-exports.run = (client, message, args) => {
-	    message.channel.send(`Louis was hast du wieder angestellt, immer machst du Unsinn!`).catch(console.error);
+exports.run = (client, message, args) => { 
+	const blame = require(`../blame.json`); 
+	var blamelist = blame.item; 
+	var rand = blamelist[Math.floor(Math.random() * blamelist.length)];
+	message.channel.send(rand).catch(console.error);
 }
