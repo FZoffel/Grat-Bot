de134aa59720ba16af0b50898ced456853aac051
Fabian Zoffel <fabian.zoffel@stepping-stone.ch>
2018-07-26 16:34:19 +0200

    Added a command to check commands

---
+++ commands/commands.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+const list = require(`../commands.json`);
+var liste = list.item;
+var endliste = []
+	for (var i=0; i < liste.length; ++i) {
+		endliste.push(`- ` + liste[i]);
+	}
+    message.channel.send(`Ich kenne folgende Befehle: `)
+    message.channel.send(endliste);
+}
